4a704da7a43fadb0cab0c4c39dc9729f8ded34ff
Sami <bmb,sami@yahoo.com>
2019-06-07 11:39:26 +0300

    data transfer object

--- src/main/java/com/reprezentantaauto/dealerautoapi/dto/CreateVehicleRequest.java
+++ src/main/java/com/reprezentantaauto/dealerautoapi/dto/CreateVehicleRequest.java
@@ -1,6 +1,5 @@
-package com.reprezentantaauto.dealerautoapi.request;
+package com.reprezentantaauto.dealerautoapi.dto;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
