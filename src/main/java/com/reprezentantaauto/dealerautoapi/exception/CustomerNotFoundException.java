caeadcfb2910390e304006261508d073e50ad90f
Sami <bmb,sami@yahoo.com>
2019-06-12 15:38:49 +0300

    creating a custom Exception for Customer

---
+++ src/main/java/com/reprezentantaauto/dealerautoapi/exception/CustomerNotFoundException.java
@@ -0,0 +1,12 @@
+package com.reprezentantaauto.dealerautoapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CustomerNotFoundException extends Exception {
+
+    public CustomerNotFoundException(String message){
+        super(message);
+    }
+}
