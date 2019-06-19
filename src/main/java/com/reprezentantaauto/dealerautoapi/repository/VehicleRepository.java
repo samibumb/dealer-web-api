723b896150add8b7f68e2f3771c430f0fd1ef7a8
Sami <bmb,sami@yahoo.com>
2019-06-19 22:13:25 +0300

    implementing signature of JpaRepository methods(@query-nativeQuery/jpql)

--- src/main/java/com/reprezentantaauto/dealerautoapi/repository/VehicleRepository.java
+++ src/main/java/com/reprezentantaauto/dealerautoapi/repository/VehicleRepository.java
@@ -2,6 +2,7 @@ package com.reprezentantaauto.dealerautoapi.repository;
 
 import com.reprezentantaauto.dealerautoapi.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface VehicleRepository extends JpaRepository<Vehicle , Long> {
 
@@ -9,5 +10,7 @@ public interface VehicleRepository extends JpaRepository<Vehicle , Long> {
 
     void deleteByIdAndBrand(long id, String brand);
 
+    //@Query(value = "SELECT Brand,Model from Vehicle ", nativeQuery = true)
+    @Query("select vehicle.brand,vehicle.model from Vehicle vehicle")
     Vehicle findByBrandAndAndModel(String brand, String model);
 }
